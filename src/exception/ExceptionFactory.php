e34924929b733720ed04ac6ec80e49bcdf96c938
Andrey <alestro@bk.ru>
2020-06-28 19:36:30 +0200

    Update and rename src/exceptions/ExceptionFactory.php to src/exception/ExceptionFactory.php

--- src/exception/ExceptionFactory.php
+++ src/exception/ExceptionFactory.php
@@ -1,39 +1,38 @@
 <?php
 
 
-namespace Lobster\Routing\Exceptions;
+namespace Bermuda\Router\Exception;
 
 
 /**
- * Class RouterException
- * @package Lobster\Routing\Exceptions
+ * Class ExceptionFactory
+ * @package Bermuda\Router\Exception
  */
 final class ExceptionFactory
 {
-
     /**
      * @param string $method
      * @param array $methods
      * @return MethodNotAllowedException
      */
-    public static function notAllows(string $method, array $allowed) : MethodNotAllowedException
+    public static function notAllows(string $method, array $allowed): MethodNotAllowedException
     {
          return new MethodNotAllowedException(sprintf('The http method : %s not allowed. Allows methods: %s.', $method, implode(', ', $allowed)), 405);
     }
 
     /**
-     * @return Exception
+     * @return RouterException
      */
-    public static function emptyHandler() : Exception
+    public static function emptyHandler(): Exception
     {
-        return new Exception('Route handler is empty.');
+        return new RouterException('Route handler is empty.');
     }
     
     /**
      * @param string $path
      * @return RouteNotFoundException
      */
-    public static function notFound() : RouteNotFoundException
+    public static function notFound(): RouteNotFoundException
     {
         return new RouteNotFoundException('', 404);
     }
