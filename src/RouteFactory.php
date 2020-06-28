176f0d1e255a26187ba58ee57d4c6810ca572ebe
Andrey <alestro@bk.ru>
2020-06-28 20:21:24 +0200

    Update RouteFactory.php

--- src/RouteFactory.php
+++ src/RouteFactory.php
@@ -1,27 +1,21 @@
 <?php
 
 
-namespace Lobster\Routing;
-
-
-use Fig\Http\Message\RequestMethodInterface;
-use Lobster\Resolver\Contracts\Resolver;
-use Lobster\Resolver\ResolverInterface;
+namespace Bermuda\Router;
 
 
 /**
  * Class RouteFactory
- * @package Lobster\Routing
+ * @package Bermuda\Router
  */
-class RouteFactory implements Contracts\RouteFactory
+class RouteFactory implements RouteFactoryInterface
 {
     /**
      * @param array $routeData
-     * @return Route
+     * @return RouteInterface
      */
-    public function __invoke(array $routeData) : Route
+    public function make(array $routeData) : RouteInterface
     {
-
         foreach (['name', 'path', 'handler'] as $key)
         {
             if(!array_key_exists($key, $routeData))
@@ -32,100 +26,21 @@ class RouteFactory implements Contracts\RouteFactory
             }
         }
 
-        return new Route($routeData['name'],
-            $routeData['path'], $routeData['handler'],
-            $routeData['methods'] ?? Route::HTTP_METHODS,
-            $routeData['tokens'] ?? Route::ROUTE_TOKENS
-        );
-    }
-
-    /**
-     * @param string $name
-     * @param string $path
-     * @param $handler
-     * @param array $tokens
-     * @return Route
-     */
-    public function get(string $name, string $path, $handler, array $tokens = Route::ROUTE_TOKENS) : Route
-    {
-        $methods = [RequestMethodInterface::METHOD_GET];
-        return ($this)(compact('name', 'path', 'handler', 'methods', 'tokens'));
-    }
-
-    /**
-     * @param string $name
-     * @param string $path
-     * @param $handler
-     * @param array $tokens
-     * @return Route
-     */
-    public function post(string $name, string $path, $handler, array $tokens = Route::ROUTE_TOKENS) : Route
-    {
-        $methods = [RequestMethodInterface::METHOD_POST];
-        return ($this)(compact('name', 'path', 'handler', 'methods', 'tokens'));
-    }
-
-    /**
-     * @param string $name
-     * @param string $path
-     * @param $handler
-     * @param array $tokens
-     * @return Route
-     */
-    public function delete(string $name, string $path, $handler, array $tokens = Route::ROUTE_TOKENS) : Route
-    {
-        $methods = [RequestMethodInterface::METHOD_DELETE];
-        return ($this)(compact('name', 'path', 'handler', 'methods', 'tokens'));
-    }
-
-    /**
-     * @param string $name
-     * @param string $path
-     * @param $handler
-     * @param array $tokens
-     * @return Route
-     */
-    public function put(string $name, string $path, $handler, array $tokens = Route::ROUTE_TOKENS) : Route
-    {
-        $methods = [RequestMethodInterface::METHOD_PUT];
-        return ($this)(compact('name', 'path', 'handler', 'methods', 'tokens'));
-    }
-
-    /**
-     * @param string $name
-     * @param string $path
-     * @param $handler
-     * @param array $tokens
-     * @return Route
-     */
-    public function head(string $name, string $path, $handler, array $tokens = Route::ROUTE_TOKENS) : Route
-    {
-        $methods = [RequestMethodInterface::METHOD_HEAD];
-        return ($this)(compact('name', 'path', 'handler', 'methods', 'tokens'));
-    }
-
-    /**
-     * @param string $name
-     * @param string $path
-     * @param $handler
-     * @param array $tokens
-     * @return Route
-     */
-    public function options(string $name, string $path, $handler, array $tokens = Route::ROUTE_TOKENS) : Route
-    {
-        $methods = [RequestMethodInterface::METHOD_OPTIONS];
-        return ($this)(compact('name', 'path', 'handler', 'methods', 'tokens'));
-    }
-
-    /**
-     * @param string $name
-     * @param string $path
-     * @param $handler
-     * @param array $tokens
-     * @return Route
-     */
-    public function any(string $name, string $path, $handler, array $tokens = Route::ROUTE_TOKENS) : Route
-    {
-        return ($this)(compact('name', 'path', 'handler', 'tokens'));
+        $route = Route($routeData['name'], $routeData['path'], $routeData['handler']);
+        
+        $route->methods($routeData['methods'] ?? Route::HTTP_METHODS);
+        $route->tokens($routeData['tokens'] ?? Route::ROUTE_TOKENS);
+        
+        if(isset($routeData['after']))
+        {
+            $route->after($routeData['after']);
+        }
+        
+        if(isset($routeData['before']))
+        {
+            $route->before($routeData['before']);
+        }
+        
+        return $route;
     }
 }
