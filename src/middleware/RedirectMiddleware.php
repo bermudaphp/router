8e4dbfe89c8f8223b78c2de96aa5bc6c631d3429
Andrey <alestro@bk.ru>
2020-08-21 13:19:17 +0200

    Create RedirectMiddleware.php

---
+++ src/middleware/RedirectMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Bermuda\Router\Middleware;
+
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+
+
+/**
+ * Class RouteMiddleware
+ * @package Bermuda\Router\Middleware
+ */
+class RedirectMiddleware implements MiddlewareInterface
+{
+    private string $reTo;
+    private bool $permanent;
+    private ResponseFactoryInterface;
+    
+    public function __construct(string $path, ResponseFactoryInterface $factory, bool $permanent = false)
+    {
+        $this->reTo = $path;
+        $this->factory = $factory;
+        $this->permanent = $permanent;
+    }
+
+     /**
+     * @inheritDoc
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $this->factory->createResponse($this->permanent ? 301 : 302)->withHeader('Location', $this->reTo);
+    }
+}
