566e095797568389d8f59c2ce5e0475a53051a33
Andrey <inbox.shelamkoff@gmail.com>
2020-10-19 00:40:34 +0300

    Update RouteNotFoundException.php

--- src/exception/RouteNotFoundException.php
+++ src/exception/RouteNotFoundException.php
@@ -8,14 +8,23 @@ namespace Bermuda\Router\Exception;
  * Class RouteNotFoundException
  * @package Bermuda\Router\Exception
  */
-class RouteNotFoundException extends RouterException
+final class RouteNotFoundException extends RouterException
 {
+    private ?string $path = null;
+    private ?string $name = null;
+    
+    public function __construct()
+    {;
+        parent::__construct('', 404);
+    }
+    
     /**
      * @param string $path
      * @return $this
      */
-    public function setPath(string $path) : self
+    public function setPath(string $path): self
     {
+        $this->path = $path;
         $this->message = sprintf('The route for the path: %s not found.', $path);
         return $this;
     }
@@ -24,9 +33,26 @@ class RouteNotFoundException extends RouterException
      * @param string $name
      * @return $this
      */
-    public function setName(string $name) : self
+    public function setName(string $name): self
     {
+        $this->name = $name;
         $this->message = sprintf('Route with name: %s not found.', $name);
         return $this;
+    }
+    
+    /**
+     * @return null|string
+     */
+    public function getName():? string
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @return null|string
+     */
+    public function getPath():? string
+    {
+        return $this->path;
     }
 }
