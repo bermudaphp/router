53f22c4a52c141db91ccb0ac1b326d242875ecc9
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 21:33:51 +0200

    Update Resource.php

--- src/Resource.php
+++ src/Resource.php
@@ -7,13 +7,84 @@ use Psr\Http\Message\ServerRequestInterface;
 
 abstract class Resource
 {
-    abstract public function show(ServerRequestInterface $request): ResponseInterface ;
+    public static function register(RouteMap $routes): RouteMap
+    {
+        $routes = static::registerGetHandler($routes);
+        $routes = static::registerCreateHandler($routes);
+        $routes = static::registerUpdateHandler($routes);
+        $routes = static::registerEditHandler($routes);
+        $routes = static::registerDestroyHandler($routes);
+        return static::registerStoreHandler($routes);
+    }
+
+    abstract public function get(ServerRequestInterface $request): ResponseInterface ;
     abstract public function create(ServerRequestInterface $request): ResponseInterface ;
     abstract public function update(ServerRequestInterface $request): ResponseInterface ;
     abstract public function delete(ServerRequestInterface $request): ResponseInterface ;
     abstract public function edit(ServerRequestInterface $request): ResponseInterface ;
-    abstract public function get(ServerRequestInterface $request): ResponseInterface ;
     abstract public function store(ServerRequestInterface $request): ResponseInterface ;
-    
-    abstract public static function register(RouteMap $routes): RouteMap ;
+
+    public static function getName(): string
+    {
+        throw new \RuntimeException('Overwrite current method');
+    }
+
+    public static function getPathPrefix(): string
+    {
+        throw new \RuntimeException('Overwrite current method');
+    }
+
+    /**
+     * @param RouteMap $routes
+     * @return RouteMap
+     */
+    public static function registerGetHandler(RouteMap $routes): RouteMap
+    {
+        return $routes->get(static::getName().'.get', static::getPathPrefix().'/?{id}', static::class . '@get');
+    }
+
+    /**
+     * @param RouteMap $routes
+     * @return RouteMap
+     */
+    public static function registerEditHandler(RouteMap $routes): RouteMap
+    {
+        return $routes->get(static::getName().'.edit', static::getPathPrefix().'/edit/{id}', static::class . '@edit');
+    }
+
+    /**
+     * @param RouteMap $routes
+     * @return RouteMap
+     */
+    public static function registerDestroyHandler(RouteMap $routes): RouteMap
+    {
+        return $routes->delete(static::getName().'.destroy', static::getPathPrefix().'/{id}', static::class . '@destroy');
+    }
+
+    /**
+     * @param RouteMap $routes
+     * @return RouteMap
+     */
+    public static function registerStoreHandler(RouteMap $routes): RouteMap
+    {
+        return $routes->post(static::getName().'.store', static::getPathPrefix(), static::class . '@store');
+    }
+
+    /**
+     * @param RouteMap $routes
+     * @return RouteMap
+     */
+    public static function registerCreateHandler(RouteMap $routes): RouteMap
+    {
+        return $routes->get(static::getName().'.create', static::getPathPrefix().'/create', static::class . '@create');
+    }
+
+    /**
+     * @param RouteMap $routes
+     * @return RouteMap
+     */
+    public static function registerUpdateHandler(RouteMap $routes): RouteMap
+    {
+        return $routes->any(['name' => static::getName(), 'methods' => ['PUT', 'PATCH']].'.update', static::getPathPrefix().'/{id}', static::class . '@update');
+    }
 }
