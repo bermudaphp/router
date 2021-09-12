41118a2f5abfd7167e01e843c7ea4686ea53ac88
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 21:46:39 +0200

    Update MatchRouteMiddleware.php

--- src/middleware/MatchRouteMiddleware.php
+++ src/middleware/MatchRouteMiddleware.php
@@ -2,19 +2,17 @@
 
 namespace Bermuda\Router\Middleware;
 
+use Bermuda\Router\Route;
 use Bermuda\Router\RouterInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Bermuda\MiddlewareFactory\MiddlewareFactoryInterface;
 
-/**
- * Class MatchRouteMiddleware
- * @package Bermuda\Router\Middleware
- */
 final class MatchRouteMiddleware implements MiddlewareInterface
 {
+    private static ?Route $route = null;
     private RouterInterface $router;
     private MiddlewareFactoryInterface $factory;
 
@@ -24,15 +22,19 @@ final class MatchRouteMiddleware implements MiddlewareInterface
         $this->factory = $factory;
     }
 
+    public static function getRoute(): ?Route
+    {
+        return self::$route;
+    }
+
     /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $route = $this->router->match($request->getMethod(), (string) $request->getUri());
+        $route = $this->router->match($request->getMethod(), (string)$request->getUri());
 
-        foreach ($route->getAttributes() as $name => $value)
-        {
+        foreach ($route->getAttributes() as $name => $value) {
             $request = $request->withAttribute($name, $value);
         }
 
