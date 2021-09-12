81871458a2e859d3d0431c08a284afcfd022128b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 21:41:26 +0200

    Update RouterFactory.php

--- src/RouterFactory.php
+++ src/RouterFactory.php
@@ -8,11 +8,36 @@ final class RouterFactory
 {
     public function __invoke(ContainerInterface $container): Router
     {
-        return new Router(
-            $container->get(Matcher::class),
-            $container->get(Generator::class),
-            $container->get(RouteMap::class)
+        return new Router($this->matcher($container),
+            $this->generator($container), $this->routeMap($container)
         );
+    }
+
+    private function matcher(ContainerInterface $container): Matcher
+    {
+        if ($container->has(Matcher::class)) {
+            return $container->get(Matcher::class);
+        }
+
+        return new RouteMatcher();
+    }
+
+    private function generator(ContainerInterface $container): Matcher
+    {
+        if ($container->has(Generator::class)) {
+            return $container->get(Generator::class);
+        }
+
+        return new PathGenerator();
+    }
+
+    private function routeMap(ContainerInterface $container): Matcher
+    {
+        if ($container->has(RouteMap::class)) {
+            return $container->get(RouteMap::class);
+        }
+
+        return new Routes();
     }
 }
 
