4fdf5b73e292877e149274e439c981bab2c3b9d8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 20:53:26 +0200

    Update Resource.php

--- src/Resource.php
+++ src/Resource.php
@@ -5,13 +5,15 @@ namespace Bermuda\Router;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-interface Resource
+abstract class Resource
 {
-    public function show(ServerRequestInterface $request): ResponseInterface ;
-    public function create(ServerRequestInterface $request): ResponseInterface ;
-    public function update(ServerRequestInterface $request): ResponseInterface ;
-    public function delete(ServerRequestInterface $request): ResponseInterface ;
-    public function edit(ServerRequestInterface $request): ResponseInterface ;
-    public function get(ServerRequestInterface $request): ResponseInterface ;
-    public function store(ServerRequestInterface $request): ResponseInterface ;
+    abstract public function show(ServerRequestInterface $request): ResponseInterface ;
+    abstract public function create(ServerRequestInterface $request): ResponseInterface ;
+    abstract public function update(ServerRequestInterface $request): ResponseInterface ;
+    abstract public function delete(ServerRequestInterface $request): ResponseInterface ;
+    abstract public function edit(ServerRequestInterface $request): ResponseInterface ;
+    abstract public function get(ServerRequestInterface $request): ResponseInterface ;
+    abstract public function store(ServerRequestInterface $request): ResponseInterface ;
+    
+    abstract public static function register(RouteMap $routes): RouteMap ;
 }
