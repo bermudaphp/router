1c175bdb6187664229851b4cbd36acd9d9cddc5d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 22:02:01 +0200

    Update RouterException.php

--- src/exception/RouterException.php
+++ src/exception/RouterException.php
@@ -1,13 +1,7 @@
 <?php
 
-
 namespace Bermuda\Router\Exception;
 
-
-/**
- * Class RouterException
- * @package Bermuda\Router\Exception
- */
 class RouterException extends \RuntimeException
 {
     /**
