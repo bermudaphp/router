aa7dd0081b5e35127a25ecbbaee2712ec1007ca3
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 15:39:29 +0200

    Update MethodNotAllowedException.php

--- src/exception/MethodNotAllowedException.php
+++ src/exception/MethodNotAllowedException.php
@@ -3,12 +3,7 @@
 namespace Bermuda\Router\Exception;
 
 use Throwable;
-use Bermuda\Router\RouteInterface;
 
-/**
- * Class MethodNotAllowedException
- * @package Bermuda\Router\Exception
- */
 final class MethodNotAllowedException extends RouterException
 {
     private string $path;
@@ -18,9 +13,10 @@ final class MethodNotAllowedException extends RouterException
     public function __construct(string $path, string $requestMethod, array $allowedMethods)
     {
         $this->path = $path;
-        $this->requestMethod = $requestMethod;
+        $this->requestMethod = strtoupper($requestMethod);
+        $this->allowedMethods = array_map('strtoupper', $allowedMethods);
         parent::__construct(sprintf('The http method : %s for path: %s not allowed. Allows methods: %s.',
-            $requestMethod, $path, implode(', ', $allowedMethods)
+            $requestMethod, $path, implode(', ', $this->allowedMethods)
         ), 405);
     }
 
@@ -48,7 +44,7 @@ final class MethodNotAllowedException extends RouterException
      */
     public function addAllowedMethods(array $methods): void
     {
-        $this->allowedMethods = array_unique(array_merge($this->allowedMethods, $methods));
+        $this->allowedMethods = array_unique(array_merge($this->allowedMethods, array_map('strtoupper', $methods)));
         $this->message = sprintf('The http method : %s for path: %s not allowed. Allows methods: %s.',
             $this->requestMethod, $this->path, implode(', ', $this->allowedMethods)
         );
