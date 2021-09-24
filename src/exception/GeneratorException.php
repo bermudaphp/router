fcd73ca73a69572e833c0851cde1931da8de5702
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 15:34:58 +0200

    Update GeneratorException.php

--- src/exception/GeneratorException.php
+++ src/exception/GeneratorException.php
@@ -1,13 +1,7 @@
 <?php
 
-
 namespace Bermuda\Router\Exception;
 
-
-/**
- * Class GeneratorException
- * @package Bermuda\Router\Exception
- */
 class GeneratorException extends RouterException
 {
 }
