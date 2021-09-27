8883b589bee16f47234d6054cc89630de5f21b2a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-28 00:39:04 +0200

    Update MatchRouteMiddleware.php

--- src/middleware/MatchRouteMiddleware.php
+++ src/middleware/MatchRouteMiddleware.php
@@ -16,24 +16,25 @@ final class MatchRouteMiddleware implements MiddlewareInterface
     public function __construct(private MiddlewareFactoryInterface $middlewareFactory, private Router $router)
     {
     }
-    
+
     /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $route = $this->router->match($request->getMethod(), (string) $request->getUri());
+        self::$route = $this->router->match($request->getMethod(), (string) $request->getUri());
 
-        foreach ($route->getAttributes() as $name => $value) {
+        foreach (self::$route->getAttributes() as $name => $value) {
             $request = $request->withAttribute($name, $value);
         }
-
-        $route = new RouteMiddleware($this->middlewareFactory, $route);
-        $request = $request->withAttribute(RouteMiddleware::class, $route);
+        
+        $request = $request->withAttribute(RouteMiddleware::class, 
+            new RouteMiddleware($this->middlewareFactory, self::$route)
+        );
 
         return $handler->handle($request);
     }
-    
+
     public static function getRoute():? Route
     {
         return self::$route;
