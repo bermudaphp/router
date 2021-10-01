228a34a47282f58457f0663d4ef3adede1e021fd
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-02 00:22:46 +0200

    Update Matcher.php

--- src/Matcher.php
+++ src/Matcher.php
@@ -5,11 +5,11 @@ namespace Bermuda\Router;
 interface Matcher
 {
     /**
-     * @param RouteMap $routes
+     * @param Route[] $routes
      * @param string $requestMethod
      * @param string $uri
      * @throws Exception\RouteNotFoundException
      * @throws Exception\MethodNotAllowedException
      */
-    public function match(RouteMap $routes, string $requestMethod, string $uri): Route ;
+    public function match(iterable $routes, string $requestMethod, string $uri): Route ;
 }
