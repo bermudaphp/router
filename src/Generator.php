5f0d2b31ae77360a9aca267030e2818dfcce273b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-02 00:25:02 +0200

    Update Generator.php

--- src/Generator.php
+++ src/Generator.php
@@ -5,12 +5,12 @@ namespace Bermuda\Router;
 interface Generator
 {
     /**
-     * @param Route[] $routes
+     * @param RouteMap $routes
      * @param string $name
      * @param array $attributes
      * @return string
      * @throws Exception\RouteNotFoundException
      * @throws Exception\GeneratorException
      */
-    public function generate(iterable $routes, string $name, array $attributes = []): string ;
+    public function generate(RouteMap $routes, string $name, array $attributes = []): string ;
 }
