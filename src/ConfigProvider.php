7bb6bf9bd1038b7e1fc4509b80f1e1e5b706b6f9
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-02 15:34:02 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -12,12 +12,6 @@ final class ConfigProvider extends \Bermuda\Config\ConfigProvider
      */
     protected function getFactories(): array
     {
-        return [
-            Router::class => static fn(ContainerInterface $container): Router => new Router(
-                cget($container, Matcher::class, static fn() => new RouteMatcher, true),
-                cget($container, Generator::class, static fn() => new PathGenerator, true),
-                cget($container, RouteMap::class, static fn() => new Routes, true)
-            )
-        ];
+        return [Router::class => '\Bermuda\Router\RouterFactory::createRouter'];
     }
 }
