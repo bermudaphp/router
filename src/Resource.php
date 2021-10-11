a981cba39c8bf2139885e69f658284dfd0d17d96
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-12 00:27:30 +0200

    Update Resource.php

--- src/Resource.php
+++ src/Resource.php
@@ -13,17 +13,13 @@ abstract class Resource
         $routes = static::registerGetHandler($routes);
         $routes = static::registerCreateHandler($routes);
         $routes = static::registerUpdateHandler($routes);
-        $routes = static::registerEditHandler($routes);
-        $routes = static::registerDestroyHandler($routes);
-        return static::registerStoreHandler($routes);
+        return static::registerDestroyHandler($routes);
     }
 
     abstract public function get(ServerRequestInterface $request): ResponseInterface ;
     abstract public function create(ServerRequestInterface $request): ResponseInterface ;
     abstract public function update(ServerRequestInterface $request): ResponseInterface ;
     abstract public function delete(ServerRequestInterface $request): ResponseInterface ;
-    abstract public function edit(ServerRequestInterface $request): ResponseInterface ;
-    abstract public function store(ServerRequestInterface $request): ResponseInterface ;
 
     public static function getName(): string
     {
@@ -43,16 +39,7 @@ abstract class Resource
     {
         return $routes->get(static::getName().'.get', static::getPathPrefix().'/?{id}', static::class . '@get');
     }
-
-    /**
-     * @param RouteMap $routes
-     * @return RouteMap
-     */
-    public static function registerEditHandler(RouteMap $routes): RouteMap
-    {
-        return $routes->get(static::getName().'.edit', static::getPathPrefix().'/edit/{id}', static::class . '@edit');
-    }
-
+    
     /**
      * @param RouteMap $routes
      * @return RouteMap
@@ -61,16 +48,7 @@ abstract class Resource
     {
         return $routes->delete(static::getName().'.destroy', static::getPathPrefix().'/{id}', static::class . '@destroy');
     }
-
-    /**
-     * @param RouteMap $routes
-     * @return RouteMap
-     */
-    public static function registerStoreHandler(RouteMap $routes): RouteMap
-    {
-        return $routes->post(static::getName().'.store', static::getPathPrefix(), static::class . '@store');
-    }
-
+    
     /**
      * @param RouteMap $routes
      * @return RouteMap
