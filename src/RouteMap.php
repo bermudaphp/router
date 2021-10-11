a6ea87edcf6a4685f8d4f58dfada946f1e63a2b9
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-11 20:08:01 +0200

    Update RouteMap.php

--- src/RouteMap.php
+++ src/RouteMap.php
@@ -21,122 +21,105 @@ interface RouteMap extends Arrayable, IteratorAggregate
     public function route(string $name): Route;
 
     /**
-     * @param Route|array $route
-     * @return RouteMap
-     */
-    public function add(Route|array $route): RouteMap;
-
-    /**
-     * @param string|array $prefix
-     * @param callable $callback
-     * @return RouteMap
-     */
-    public function group(string|array $prefix, callable $callback): RouteMap;
-
-    /**
-     * @param string|array $name
-     * @param string|null $path
-     * @param mixed $handler
+     * @param string $prefix
+     * @param mixed $middleware
      * @param array|null $tokens
-     * @param array|null $middleware
+     * @param callable|null $callback
+     * @throws \InvalidArgumentException if $callback is null
+     * @return RouteMap
+     */
+    public function group(string $prefix, mixed $middleware = null, ?array $tokens = null, callable $callback = null): RouteMap;
+
+    /**
+     * @param string $name
+     * @param string $path
+     * @param $handler
+     * @param array|null $tokens
+     * @param mixed|null $middleware
      * @return RouteMap
      */
     public function get(
-        string|array $name,
-        ?string      $path = null,
-                     $handler = null,
-        ?array       $tokens = null,
-        ?array       $middleware = null): RouteMap;
+        string $name, string $path,
+        $handler, ?array $tokens = null,
+        mixed $middleware = null): RouteMap;
 
     /**
-     * @param string|array $name
-     * @param string|null $path
-     * @param mixed $handler
+     * @param string $name
+     * @param string $path
+     * @param $handler
      * @param array|null $tokens
-     * @param array|null $middleware
+     * @param mixed|null $middleware
      * @return RouteMap
      */
     public function post(
-        string|array $name,
-        ?string      $path = null,
-                     $handler = null,
-        ?array       $tokens = null,
-        ?array       $middleware = null): RouteMap;
+        string $name, string $path,
+        $handler, ?array $tokens = null,
+        mixed $middleware = null): RouteMap;
 
     /**
-     * @param string|array $name
-     * @param string|null $path
-     * @param mixed $handler
+     * @param string $name
+     * @param string $path
+     * @param $handler
      * @param array|null $tokens
-     * @param array|null $middleware
+     * @param mixed|null $middleware
      * @return RouteMap
      */
     public function delete(
-        string|array $name,
-        ?string      $path = null,
-                     $handler = null,
-        ?array       $tokens = null,
-        ?array       $middleware = null): RouteMap;
+        string $name, string $path,
+               $handler, ?array $tokens = null,
+        mixed $middleware = null): RouteMap;
 
     /**
-     * @param string|array $name
-     * @param string|null $path
-     * @param mixed $handler
+     * @param string $name
+     * @param string $path
+     * @param $handler
      * @param array|null $tokens
-     * @param array|null $middleware
+     * @param mixed|null $middleware
      * @return RouteMap
      */
     public function put(
-        string|array $name,
-        ?string      $path = null,
-                     $handler = null,
-        ?array       $tokens = null,
-        ?array       $middleware = null): RouteMap;
+        string $name, string $path,
+        $handler, ?array $tokens = null,
+        mixed $middleware = null): RouteMap;
 
     /**
-     * @param string|array $name
-     * @param string|null $path
-     * @param mixed $handler
+     * @param string $name
+     * @param string $path
+     * @param $handler
      * @param array|null $tokens
-     * @param array|null $middleware
+     * @param mixed|null $middleware
      * @return RouteMap
      */
     public function patch(
-        string|array $name,
-        ?string      $path = null,
-                     $handler = null,
-        ?array       $tokens = null,
-        ?array       $middleware = null): RouteMap;
+        string $name, string $path,
+        $handler, ?array $tokens = null,
+        mixed $middleware = null): RouteMap;
 
     /**
-     * @param string|array $name
-     * @param string|null $path
-     * @param mixed $handler
+     * @param string $name
+     * @param string $path
+     * @param $handler
      * @param array|null $tokens
-     * @param array|null $middleware
+     * @param mixed|null $middleware
      * @return RouteMap
      */
     public function options(
-        string|array $name,
-        ?string      $path = null,
-                     $handler = null,
-        ?array       $tokens = null,
-        ?array       $middleware = null): RouteMap;
+        string $name, string $path,
+        $handler, ?array $tokens = null,
+        mixed $middleware = null): RouteMap;
 
     /**
-     * @param string|array $name
-     * @param string|null $path
-     * @param mixed $handler
+     * @param string $name
+     * @param string $path
+     * @param $handler
+     * @param array|string|null $methods
      * @param array|null $tokens
-     * @param array|null $middleware
+     * @param mixed|null $middleware
      * @return RouteMap
      */
-    public function any(
-        string|array $name,
-        ?string      $path = null,
-                     $handler = null,
-        ?array       $tokens = null,
-        ?array       $middleware = null): RouteMap;
+    public function any(string $name, string $path,
+        $handler, array|string $methods = null, ?array $tokens = null,
+        mixed $middleware = null): RouteMap;
 
     /**
      * @param string $resource must be subclass of Resource
