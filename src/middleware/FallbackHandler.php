aad7ea24b9ebb59274a237effe4a0d44483fc619
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-05-20 12:40:26 +0200

    Create FallbackHandler.php

---
+++ src/middleware/FallbackHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Berumda\Router\Middleware;
+
+use Bermuda\Router\Exception\RouteNotFoundException;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class FallbackHandler implements RequestHandlerInterface
+{
+    public function __construct(
+        private readonly ResponseFactoryInterface $responseFactory,
+        private readonly bool $exceptionMode = false
+    ) {
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        if ($this->exceptionMode) {
+            throw new RouteNotFoundException((string)$request->getUri(), $request->getMethod());
+        }
+
+        $response = $this->responseFactory->createResponse(404);
+        $response->getBody()->write(json_encode(['message' => 'Endpoint not found.']));
+        
+        return $response;
+    }
+}
