119b878bd065d1e1bc8a15f2bbdbe6a6a7a1ef67
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-30 23:26:18 +0200

    Update Cacheable.php

--- src/Cacheable.php
+++ src/Cacheable.php
@@ -4,6 +4,6 @@ namespace Bermuda\Router;
 
 interface Cacheable
 {
-    public function cache(string $filename, callable $fileWriter = null): void ;
-    public static function createFromCache(string $filename, array $context = null): RouteMap ;
+    public function cache(string $filename, ?callable $fileWriter = null): void ;
+    public static function createFromCache(string $filename, ?array $context = null): RouteMap ;
 }
