60126001afa1d7ffdb91f9be2adcb5fc40e9b9ce
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-06-12 20:41:38 +0200

    Delete src directory

--- src/Cacheable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bermuda\Router;
-
-interface Cacheable
-{
-    public function cache(string $filename, ?callable $fileWriter = null): void ;
-    public static function createFromCache(string $filename, ?array $context = null): RouteMap ;
-}
--- src/Matcher.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Bermuda\Router;
-
-interface Matcher
-{
-    public function match(RouteMap $routes, string $uri, string $requestMethod):? RouteRecord;
-}
